b93136115c667a690a7d3972beaa5567d29b7821
Brittany Lee <BrittanyLee2015@u.northwestern.edu>
2015-03-09 23:43:18 -0500

    get steps

--- scripts/recipe.py
+++ scripts/recipe.py
@@ -11,20 +11,23 @@ def fetch_recipe(url):
 
 	# get name of recipe
 	name = soup.find(id='itemTitle').string
+	print 'RECIPE: ' + name
 
 	# get times
-	prep_time = soup.find(id='prepMinsSpan').string
-	cook_time = soup.find(id='cookMinsSpan').string
-	total_time = soup.find(id='totalMinsSpan').string
+	# prep_time = soup.find(id='prepMinsSpan').string
+	# cook_time = soup.find(id='cookMinsSpan').string
+	# total_time = soup.find(id='totalMinsSpan').string
+
+	# print 'PREP TIME: ' + prep_time
+	# print 'COOK TIME: ' + cook_time
+	# print 'TOTAL TIME: ' + total_time
 
 	# get ingredients
+	ingredients = []
 	ingredient_list = soup.find_all(id='liIngredient')
 	for ingredient in ingredient_list:
 		amount_string = ingredient.find(id='lblIngAmount').string
 		name_string = ingredient.find(id='lblIngName').string
-
-		print name_string
-		print amount_string
 
 		amount_string = amount_string.split()
 		if len(amount_string) > 1:
@@ -32,6 +35,7 @@ def fetch_recipe(url):
 			measurement = amount_string[1]
 		else:
 			quantity = amount_string[0]
+			measurement = ''
 
 		name_string = name_string.split(',')
 		ingredient_name = name_string[0]
@@ -41,24 +45,58 @@ def fetch_recipe(url):
 		ingredient_name = ingredient_name.split()
 		tagged_name = nltk.pos_tag(ingredient_name)
 
+		descriptor = ''
 		if len(tagged_name) > 1:
-			if tagged_name[0][1] != 'NNP':
-				name = tagged_name[1][0]
-
-		print
-		print name
-		print
+			if tagged_name[0][1] == 'VBD':
+				descriptor = ingredient_name[0]
+				del ingredient_name[0]
+				name_ingr = ' '.join(ingredient_name)
+			else:
+				name_ingr = ' '.join(ingredient_name)
+		else:
+			name_ingr = ingredient_name[0]
 
 		preparation = []
 		if len(name_string) > 0:
 			for phrase in name_string:
 				preparation.append(phrase)
+		preparation = ''.join(preparation)
 			
-		print
-		print ingredient_name
-		print quantity
-		print measurement
-		print preparation
-		print
+		ingredient_obj = {}
+		ingredient_obj['name'] = name_ingr
+		ingredient_obj['descriptor'] = descriptor
+		ingredient_obj['quantity'] = quantity
+		ingredient_obj['measurement'] = measurement
+		ingredient_obj['preparation'] = preparation
+
+		ingredients.append(ingredient_obj)
+		# print
+		# print 'ingredient: ' + name_ingr
+		# print 'descriptor: ' + descriptor
+		# print 'quantity: ' + quantity
+		# print 'measurement: ' + measurement
+		# print 'preparation: ' + preparation
+		# print
+
+	# get steps
+	instruction_div = soup.find('div', {'class': 'directions'})
+	instructions = instruction_div.find_all('li')
+	instruction_list = []
+	for step in instructions:
+		step_obj = {}
+		step_obj['text'] = step.span.text
+
+		instruction_list.append(step_obj)
+		# print step.span.text
+		# print
+
+
+
+
+
+
+
+
+
 
 fetch_recipe(url)   
